974e4b216d638e19d901c9c0c64fe440389e9d88
Paulo <paulodiniz019283@gmail.com>
2022-10-11 08:49:39 -0400

    Lista de Tarefas
    Criando, Editando e Excluindo tarefas

---
+++ lista/src/components/Main.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import './Main.css'
+
+
+//Form 
+import { FaPlus } from 'react-icons/fa'
+
+//Tarefas 
+import { FaEdit, FaWindowClose } from 'react-icons/fa'
+
+
+
+export default class Main extends Component {
+
+    state = {
+        novaTarefa: '',
+        tarefas: [],
+        index: -1,
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { tarefas, index } = this.state;
+        let { novaTarefa } = this.state;
+        novaTarefa = novaTarefa.trim();
+
+        if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+        const novasTarefas = [...tarefas];
+
+        if (index === -1) {
+            this.setState({
+                tarefas: [...novasTarefas, novaTarefa],
+                novaTarefa: ''
+            })
+        } else {
+            novasTarefas[index] = novaTarefa;
+
+            this.setState({
+                tarefas: [...novasTarefas],
+                index: -1,
+            })
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            novaTarefa: e.target.value,
+        });
+    }
+
+    handleEdit = (e, index) => {
+        const { tarefas } = this.state
+
+        this.setState({
+            index,
+            novaTarefa: tarefas[index],
+        })
+    }
+
+    handleDelete = (e, index) => {
+        const { tarefas } = this.state
+        const novaTarefas = [...tarefas]
+        novaTarefas.splice(index, 1)
+
+        this.setState({
+            tarefas: [...novaTarefas],
+        })
+    }
+    render() {
+        // eslint-disable-next-line no-unused-vars
+        const { novaTarefa, tarefas } = this.state;
+
+        return (
+            <div className="main">
+                <h1>Lista de Tarefas</h1>
+
+                <form onSubmit={this.handleSubmit} action="#" className="form">
+                    <input
+                        onChange={this.handleChange}
+                        type="text"
+                        value={novaTarefa}
+                    />
+                    <button type="submit">
+                        <FaPlus />
+                    </button>
+                </form>
+
+                <ul className="tarefas">
+                    {tarefas.map((tarefa, index) => (
+                        <li key={tarefa}>
+                            {tarefa}
+                            <span>
+                                <FaEdit
+                                    onClick={(e) => this.handleEdit(e, index)}
+                                    className="edit"
+                                />
+
+                                <FaWindowClose
+                                    onClick={(e) => this.handleDelete(e, index)}
+                                    className="delete"
+                                />
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
+}
